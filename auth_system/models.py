438fbafd8ecac99ae9cef6a82dd8ba60228a08d4
David <david2009mn@gmail.com>
2024-05-07 22:12:55 +0300

    Added CustomUser model

--- auth_system/models.py
+++ auth_system/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ACCESS_CHOICES = [
+        ('user', 'User'),
+        ('moder', 'Moderator'),
+        ('admin', 'Admin')
+    ]
+
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='user-avatars/',  blank=True, null=False, default='avatars/default/default.png')
+    access = models.CharField(max_length=31, choices=ACCESS_CHOICES, default='user')
+
+    def __str__(self):
+        return self.username
+    
+    class Meta():
+        verbose_name = 'User'
+
+
