031bdb899f3e87f005e92752660ea1aebb66aad4
David <david2009mn@gmail.com>
2024-05-07 22:19:01 +0300

    Added register view

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from forms import RegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "auth_sys/register.html"
+    success_url = reverse_lazy("tasks:task-list")
+
+    def form_valid(self, form):
+        user = form.save()
+    
+        if user:
+            login(self.request, user)
+            if user.is_authenticated:
+                return HttpResponseRedirect(self.success_url)
+            else:
+                return redirect("auth_sys:login")
+
+        return super().form_valid(form)
+
+
