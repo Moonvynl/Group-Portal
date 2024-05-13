e6b82ab37698f1da6d3bb41caa66edf81b56fec6
Michael322312 <zomisha0808@gmail.com>
2024-05-13 21:54:00 +0300

    Remade calendar view

--- calendar_events/views.py
+++ calendar_events/views.py
@@ -2,20 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView, TemplateView, ListView, DetailView
 from calendar_events.models import Event
 from django.utils import timezone
 from django.template.defaulttags import register
 import calendar
+import datetime
 
-class CalendarEventListView(ListView):
-    model = Event
-    context_object_name = "events"
-    template_name = "calendar_events/calendar_page.html"
 
-    def get_context_data(self,*args, **kwargs):
-        context = super(CalendarEventListView, self).get_context_data(*args,**kwargs)
+def view_date_events(request, day=None, month=None, year=None):
+    if not day or not month or not year:
         today = timezone.now()
-        context['calendar'] = calendar.Calendar().monthdays2calendar(today.year, today.month)
-        context['today'] = today
-        return context
+        date = datetime.datetime(today.year, today.month, today.day)
+    else:
+        date = datetime.datetime(year, month, day)
+
+    events_date = Event.objects.filter(event_date=date)
+
+    context = {
+        "calendar": calendar.Calendar().monthdays2calendar(date.year, date.month),
+        "events": Event.objects.all(),
+        "date": date,
+        "events_date": events_date
+    }
+
+    return render(
+        template_name="calendar_events/calendar_page.html",
+        context=context,
+        request=request
+    )
