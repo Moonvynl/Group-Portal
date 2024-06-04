2d464844cb1c184465fca4417e575d12301c29de
Michael322312 <zomisha0808@gmail.com>
2024-06-04 12:46:34 +0300

    Registered models

--- photo_gallery/admin.py
+++ photo_gallery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from photo_gallery.models import PhotoAuth, PhotoPost
 # Register your models here.
+
+admin.site.register(PhotoAuth)
+admin.site.register(PhotoPost)
