eef0ae0c506c14495c3cf3c421a02ee5e1f0ae61
Michael322312 <zomisha0808@gmail.com>
2024-06-05 16:27:05 +0300

    Urls for views

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.urls import reverse_lazy
-from photo_gallery.views import PhotoPostCreateView, PhotoAuthCreateView, PhotoPostListView
+from photo_gallery.views import PhotoPostCreateView, PhotoAuthCreateView, PhotoAuthListView, PhotoUserListView, StaffPostSubmitListView
 
 urlpatterns = [
-    path('photo_posts/', PhotoPostListView.as_view(), name='photo_posts'),
+    path('photo_posts/', PhotoAuthListView.as_view(), name='photo_posts'),
+    path('photo_posts/user/', PhotoUserListView.as_view(), name='user_photos'),
     path('create_photo/', PhotoPostCreateView.as_view(), name='create_photo'),
-    path('auth_request/photopost/<int:pk>', PhotoAuthCreateView.as_view(), name='auth_photo')
+    path('auth_request/photopost/<int:pk>', PhotoAuthCreateView.as_view(), name='auth_photo'),
+    path('post_auth_list/', StaffPostSubmitListView.as_view(), name='post_auth_list'),
 ]
 
 app_name = 'photo_post'
