52a00f492adc9e1c3a8ca7ccd4f0eb55611282d1
Michael322312 <zomisha0808@gmail.com>
2024-06-07 12:19:07 +0300

    Added staff check up form

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import reverse_lazy
-from photo_gallery.views import PhotoPostCreateView, PhotoAuthCreateView, PhotoAuthListView, PhotoUserListView, StaffPostSubmitListView
+from photo_gallery.views import PhotoPostCreateView, PhotoAuthCreateView, PhotoAuthListView, PhotoUserListView, StaffPostSubmitListView, StaffPostUpdateView
 
 urlpatterns = [
     path('photo_posts/', PhotoAuthListView.as_view(), name='photo_posts'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create_photo/', PhotoPostCreateView.as_view(), name='create_photo'),
     path('auth_request/photopost/<int:pk>', PhotoAuthCreateView.as_view(), name='auth_photo'),
     path('post_auth_list/', StaffPostSubmitListView.as_view(), name='post_auth_list'),
+    path('staff_auth_form/<int:pk>', StaffPostUpdateView.as_view(), name='staff_auth_form')
 ]
 
 app_name = 'photo_post'
